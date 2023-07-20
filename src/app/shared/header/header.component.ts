6df3acd44ba6d077661eaad57336f43203607566
Hemant Basnet <hemantbasnet61@gmil.com>
2023-07-20 15:57:12 +0545

    vision

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+// Interfaces to define the structure of menu items and sub-menu items
+interface SubMenuItem {
+  label: string;
+  routerLink: string;
+}
+
+interface MenuItem {
+  label: string;
+  submenuItems?: SubMenuItem[];
+  routerLink?: string;
+  showDropdown?: boolean;
+}
 
 @Component({
   selector: 'app-header',
@@ -6,5 +19,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  // Array to hold the menu items with their respective properties
+  menuItems: MenuItem[] = [
+    { label: 'Home', routerLink: '/' },
+    {
+      label: 'Product',
+      submenuItems: [
+        { label: 'Cricket', routerLink: 'profile' },
+        { label: 'Football', routerLink: 'profile' },
+        { label: 'Hockey', routerLink: 'profile' },
+      ],
+      routerLink: '/',
+    },
+    {
+      label: 'Resources',
+      submenuItems: [
+        { label: 'Cricket', routerLink: 'profile' },
+        { label: 'Football', routerLink: 'profile' },
+        { label: 'Hockey', routerLink: 'profile' },
+      ],
+      routerLink: '/',
+    },
+    { label: 'Pricing', routerLink: '/pricing' },
+  ];
 
+  // Function to toggle the visibility of submenus for a menu item:
+  toggleDropdown(menuItem: MenuItem): void {
+    this.menuItems.forEach(
+      (item) =>
+        (item.showDropdown = item === menuItem && !menuItem.showDropdown)
+    );
+  }
+
+  // Function to close all open submenus
+  closeAllDropdowns(): void {
+    this.menuItems.forEach((menuItem) => (menuItem.showDropdown = false));
+  }
+
+  // Host listener to detect clicks outside the dropdown menu and close them
+  @HostListener('document:click', ['$event.target'])
+  onClickOutside(targetElement: any): void {
+    if (!targetElement.closest('.dropdown, .anchor-tag, .dropdown-item')) {
+      this.closeAllDropdowns();
+    }
+  }
 }
